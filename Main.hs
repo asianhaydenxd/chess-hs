b2ca8f81bcd3199b31b250290045779947a098e7
Hayden <asianhaydenxd@gmail.com>
2022-04-20 17:30:53 -0400

    Create Chess game function in Main

--- Main.hs
+++ Main.hs
@@ -2,5 +2,10 @@ module Main where
 
 import Chess
 
+playChess :: IO ()
+playChess = play [] where
+    play :: Game -> IO ()
+    play = undefined
+
 main :: IO ()
-main = undefined
+main = playChess
